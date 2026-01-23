400f326b36c7b563ff80ac16bb86ae1593d75a02
GamiloFR <gamilofr.twitch@gmail.com>
2026-01-23 10:14:03 +0100

    chore: link to GamiloFR/pokeclicker repository

--- src/main.js
+++ src/main.js
@@ -45,13 +45,13 @@ function createWindow() {
   });
 
   mainWindow.setMenuBarVisibility(false);
-  mainWindow.setTitle('PokéClicker');
+  mainWindow.setTitle('PokéClicker - Custom');
 
   // Check if we've already downloaded the data, otherwise load our loading screen
-  if (fs.existsSync(`${dataDir}/pokeclicker-master/docs/index.html`)) {
-    mainWindow.loadURL(`file://${dataDir}/pokeclicker-master/docs/index.html`);
+  if (fs.existsSync(`${dataDir}/pokeclicker-custom/docs/index.html`)) {
+    mainWindow.loadURL(`file://${dataDir}/pokeclicker-custom/docs/index.html`);
   } else {
-    mainWindow.loadURL(`file://${__dirname}/pokeclicker-master/docs/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/pokeclicker-custom/docs/index.html`);
   }
 
   mainWindow.on('close', (event) => {
@@ -77,13 +77,13 @@ function createSecondaryWindow() {
   });
 
   newWindow.setMenuBarVisibility(false);
-  newWindow.setTitle('PokéClicker (alternate)');
+  newWindow.setTitle('PokéClicker - Custom (alternate)');
 
   // Check if we've already downloaded the data, otherwise load our loading screen
-  if (fs.existsSync(`${dataDir}/pokeclicker-master/docs/index.html`)) {
-    newWindow.loadURL(`file://${dataDir}/pokeclicker-master/docs/index.html`);
+  if (fs.existsSync(`${dataDir}/pokeclicker-custom/docs/index.html`)) {
+    newWindow.loadURL(`file://${dataDir}/pokeclicker-custom/docs/index.html`);
   } else {
-    newWindow.loadURL(`file://${__dirname}/pokeclicker-master/docs/index.html`);
+    newWindow.loadURL(`file://${__dirname}/pokeclicker-custom/docs/index.html`);
   }
 
   newWindow.on('close', (event) => {
@@ -113,9 +113,9 @@ app.on('activate', () => {
 /*
 DISCORD STUFF
 */
-    
+
 const isMainInstance = app.requestSingleInstanceLock()
-    
+
 if (!isMainInstance) {
   app.quit()
 } else {
@@ -191,7 +191,7 @@ if (!isMainInstance) {
   const downloadUpdate = async (initial = false) => {
     const zipFilePath = `${dataDir}/update.zip`;
     const file = fs.createWriteStream(zipFilePath);
-    https.get('https://codeload.github.com/pokeclicker/pokeclicker/zip/master', async res => {
+    https.get('https://codeload.github.com/GamiloFR/pokeclicker/zip/custom', async res => {
       let cur = 0;
       try {
         if (!initial) await mainWindow.webContents.executeJavaScript(`Notifier.notify({ title: '[UPDATER] v${newVersion}', message: 'Downloading Files...<br/><span id="update-message-progress">Please Wait...</span>', timeout: 1e6 })`);
@@ -213,7 +213,7 @@ if (!isMainInstance) {
 
         const zip = new Zip(zipFilePath);
 
-        const extracted = zip.extractEntryTo('pokeclicker-master/docs/', `${dataDir}`, true, true);
+        const extracted = zip.extractEntryTo('pokeclicker-custom/docs/', `${dataDir}`, true, true);
 
         fs.unlinkSync(zipFilePath);
 
@@ -226,7 +226,7 @@ if (!isMainInstance) {
 
         // If this is the initial download, don't ask the user about refreshing the page
         if (initial) {
-          mainWindow.loadURL(`file://${dataDir}/pokeclicker-master/docs/index.html`);
+          mainWindow.loadURL(`file://${dataDir}/pokeclicker-custom/docs/index.html`);
           return;
         }
 
@@ -239,7 +239,7 @@ if (!isMainInstance) {
         });
 
         if (userResponse == 0){
-          mainWindow.loadURL(`file://${dataDir}/pokeclicker-master/docs/index.html`);
+          mainWindow.loadURL(`file://${dataDir}/pokeclicker-custom/docs/index.html`);
         }
       });
     }).on('error', (e) => {
@@ -266,7 +266,7 @@ if (!isMainInstance) {
   }
 
   const checkForUpdates = () => {
-    const request = https.get('https://raw.githubusercontent.com/pokeclicker/pokeclicker/master/package.json', res => {
+    const request = https.get('https://raw.githubusercontent.com/GamiloFR/pokeclicker/custom/package.json', res => {
       let body = '';
 
       res.on('data', d => {
@@ -288,7 +288,7 @@ if (!isMainInstance) {
           }
         }catch(e) {}
       });
-    
+
     }).on('error', (e) => {
       // TODO: Update download failed
       console.warn('Couldn\'t check for updated version, might be offline..');
@@ -303,7 +303,7 @@ if (!isMainInstance) {
       buttons: ['Update Now', 'Remind Me', 'No (disable check)'],
       noLink: true,
     });
-    
+
     switch (userResponse) {
       case 0:
         downloadUpdate();
@@ -327,7 +327,7 @@ if (!isMainInstance) {
 
   try {
     // If we can get our current version, start checking for updates once the game starts
-    currentVersion = JSON.parse(fs.readFileSync(`${dataDir}/pokeclicker-master/docs/package.json`).toString()).version;
+    currentVersion = JSON.parse(fs.readFileSync(`${dataDir}/pokeclicker-custom/docs/package.json`).toString()).version;
     if (currentVersion == '0.0.0') throw Error('Must re-download updated version');
     setTimeout(() => {
       startUpdateCheckInterval(true);
@@ -347,7 +347,7 @@ if (!isMainInstance) {
         buttons: ['Restart App Now', 'Later'],
         noLink: true,
       });
-      
+
       switch (userResponse) {
         case 0:
           windowClosed = true;
